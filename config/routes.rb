d3da9cd58783001b2eda91cbe1744482f632f791
ricglz0201 <rjgcastillo@hotmail.com>
2018-06-20 22:59:50 -0500

    Changes to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'sessions#new'
 
-  root 'users#new'
+  get '/signup', to: 'users#new', as: :sign_up
+  post '/signup', to: 'users#create'
+
   get '/success', to: 'application#success', as: :success
 
-  resources :users
+  resources :sessions
 end
