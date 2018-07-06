a763c649ad7d09ff26a7717c3f6d064fe8c1190e
ricglz0201 <rjgcastillo@hotmail.com>
2018-07-05 21:59:45 -0500

    Improved logic

--- app/channels/game_channel.rb
+++ app/channels/game_channel.rb
@@ -1,6 +1,6 @@
 class GameChannel < ApplicationCable::Channel
   def subscribed
-    @username = User.find(current_user.id).username
+    @username = current_user.username
   end
 
   def unsubscribed; end
@@ -8,17 +8,19 @@ class GameChannel < ApplicationCable::Channel
   def join(data)
     if game_id = data['game_id']
       stream_from "room#{game_id}"
-      ActionCable.server.broadcast "room#{game_id}", newcommer:true if Board.find(game_id).guest != nil
+      GameJob.perform_now(game_id)
     end
   end
 
   def leave_room(data)
+    stop_all_streams
     if game_id = data['game_id']
       board = Board.find(game_id)
       username = User.find(current_user.id).username
       board.host = board.guest if board.host == username
       board.guest = nil
       board.save
+      ActionCable.server.broadcast "room#{game_id}", leaveRoom:true
     end
   end
 
